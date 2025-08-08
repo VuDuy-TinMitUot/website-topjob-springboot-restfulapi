acea7e001e5b9d483a165d83d01da3ebe4642b17
VuDuy <vuduy24122004@gmail.com>
2025-08-08 13:47:51 +0700

    chore: create init project

---
+++ src/main/java/com/vuduy/restfulapi/VuduyrestfulapiApplication.java
@@ -0,0 +1,13 @@
+package com.vuduy.restfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuduyrestfulapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuduyrestfulapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vuduy/restfulapi/VuduyrestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuduy.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuduyrestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
