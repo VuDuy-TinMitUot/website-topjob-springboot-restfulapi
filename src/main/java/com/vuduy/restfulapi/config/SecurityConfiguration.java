184f9743297090871a3b09ae6dea9298755e2339
VuDuy <vuduy24122004@gmail.com>
2025-08-10 09:10:18 +0700

    feat: enable security and default of spring security

--- src/main/java/com/vuduy/restfulapi/config/SecurityConfiguration.java
+++ src/main/java/com/vuduy/restfulapi/config/SecurityConfiguration.java
@@ -3,10 +3,15 @@ package com.vuduy.restfulapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
@@ -14,5 +19,21 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Mặc định SECURITY là stateful, trong khi ta dùng mô hình stateless
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        authz -> authz
+                                .requestMatchers("/").permitAll()
+                                .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // sử
+                                                                                                               // dụng
+                                                                                                               // mô
+                                                                                                               // hình
+                                                                                                               // stateless
+        return http.build();
     }
 }
